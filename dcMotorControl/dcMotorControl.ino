88bfe618964632ecea450e1f4d79339861de7776
eidetech <marcuseide@me.com>
2022-01-14 15:58:01 +0100

    Realized problem with only one wire per encdoer, need to change Arduino

--- dcMotorControl/dcMotorControl.ino
+++ dcMotorControl/dcMotorControl.ino
@@ -1,9 +1,7 @@
 // Left encoder pins
-#define ENCA_L 13
 #define ENCB_L 2
 
 // Right encoder pins
-#define ENCA_R 13
 #define ENCB_R 3
 
 // Left motor pins
@@ -17,17 +15,20 @@
 #define IN1_R 12
 
 volatile int posL = 0; // volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
-volatile int posR = 0;
+volatile int posR = -6574;
 
 int safePosL = 0;
 int safePosR = 0;
 
+int leftDir = 1;
+int rightDir = 1;
+
 // Kinematics
-double px = 2640/2;
-double py = 500;
+double px = 1435;
+double py = 200;
 double L1 = 0; // Length of left string mm
-double L2 = 2940; // Length of right string mm
-const double c = 2940; // Length of top aluminium bar mm
+double L2 = 2870; // Length of right string mm
+const double c = 2870; // Length of top aluminium bar mm
 const double circumference = 138.23; // Circumference of string wheel mm
 const double encoderCircumferenceRatio = circumference/342;
 
@@ -37,12 +38,10 @@ void setup() {
   Serial.begin(9600);
 
   // Left encoder setup
-  pinMode(ENCA_L,INPUT);
   pinMode(ENCB_L,INPUT);
   attachInterrupt(digitalPinToInterrupt(ENCB_L),encoder_ISR_L,RISING);
 
   // Right encoder setup
-  pinMode(ENCA_R,INPUT);
   pinMode(ENCB_R,INPUT);
   attachInterrupt(digitalPinToInterrupt(ENCB_R),encoder_ISR_R,RISING);
   
@@ -58,10 +57,63 @@ void setup() {
 }
 
 void loop() {
-
-
   //teleport();
-  goSomeplace();
+  //goSomeplace();
+  //delay(1000);
+  //jobDone = false;
+  //goHome();
+  getEncoderVals();
+  
+}
+
+void goHome()
+{
+  if(!jobDone)
+  {
+  while (safePosL > 0 || safePosR > -6574)
+  {
+    // Interrupt handling
+    safePosL = 0;
+    safePosR = 0;
+    noInterrupts(); // Disable interrupts
+    safePosL = posL;
+    safePosR = posR;
+    interrupts(); // Enable interrupts
+
+
+    setMotor(-leftDir, 150, PWM_L, IN1_L, IN2_L);
+    Serial.println(safePosL);
+    Serial.println(safePosR);
+    setMotor(-rightDir, 150, PWM_R, IN1_R, IN2_R);
+
+    if (safePosL > 0)
+    {
+      setMotor(leftDir, 0, PWM_L, IN1_L, IN2_L);
+    }
+    if (safePosR > -6574)
+    {
+      setMotor(rightDir, 0, PWM_R, IN1_R, IN2_R);
+    }
+  }
+  jobDone = true;
+  }
+}
+
+void getEncoderVals()
+{
+// Interrupt handling
+  safePosL = 0;
+  safePosR = 0;
+  noInterrupts(); // Disable interrupts
+  safePosL = posL;
+  safePosR = posR;
+  interrupts(); // Enable interrupts
+
+  Serial.print("posL = ");
+  Serial.print(posL);
+  Serial.print(" ");
+  Serial.print("posR = ");
+  Serial.println(posR);
 }
 
 void goSomeplace()
@@ -69,57 +121,67 @@ void goSomeplace()
   if(!jobDone)
   {
 
+  // Calculate lengths of strings
   L1 = sqrt(px*px+py*py) - L1;
   L2 = sqrt((c-px)*(c-px)+py*py) - L2;
 
+  //L1 = sqrt(py*py + (px+c/2) * (px+c/2));
+  //L2 = sqrt(py*py + (px-c/2) * (px-c/2));
+
+  // Print calculated lengths
   Serial.print("L1 = ");
   Serial.println(L1);
   Serial.print("L2 = ");
   Serial.println(L2);
 
-  
-
-  while (safePosL*encoderCircumferenceRatio <= L1)
+  if (L1 < 0)
   {
-  Serial.println("Inside while loop 1");
-  // Interrupt handling
-  safePosL = 0;
-  noInterrupts(); // Disable interrupts
-  safePosL = posL;
-  interrupts(); // Enable interrupts
-  setMotor(1, 150, PWM_L, IN1_L, IN2_L);
-  Serial.println(safePosL*encoderCircumferenceRatio);
-  Serial.println(L1);
-
-  if (safePosL*encoderCircumferenceRatio >= L1)
+    leftDir = -1;
+  }else if (L1 < 0)
   {
-    setMotor(2, 0, PWM_L, IN1_L, IN2_L);
-    while (safePosR*encoderCircumferenceRatio >= L2)
+    leftDir = 1;
+  }
+
+  if (L2 > 0)
+  {
+    rightDir = -1;
+  }else if (L2 < 0)
+  {
+    rightDir = 1;
+  }
+
+  while (safePosL*encoderCircumferenceRatio <= L1 || safePosR*encoderCircumferenceRatio >= L2)
+  {
+    // Interrupt handling
+    safePosL = 0;
+    safePosR = 0;
+    noInterrupts(); // Disable interrupts
+    safePosL = posL;
+    safePosR = posR;
+    interrupts(); // Enable interrupts
+
+    setMotor(leftDir, 150, PWM_L, IN1_L, IN2_L);
+    Serial.println(safePosL*encoderCircumferenceRatio);
+    Serial.println(safePosR*encoderCircumferenceRatio);
+    setMotor(rightDir, 150, PWM_R, IN1_R, IN2_R);
+    Serial.println(L1);
+
+    if (safePosL*encoderCircumferenceRatio >= L1)
     {
-      Serial.println("Inside while loop 2");
-      // Interrupt handling
-      safePosR = 0;
-      noInterrupts(); // Disable interrupts
-      safePosR = posR;
-      interrupts(); // Enable interrupts
-      setMotor(1, 150, PWM_R, IN1_R, IN2_R);
-      Serial.println(safePosR*encoderCircumferenceRatio);
-      Serial.println(L2);
-        if (safePosR*encoderCircumferenceRatio <= L2)
-        {
-          setMotor(2, 0, PWM_R, IN1_R, IN2_R);
-        }
+      setMotor(leftDir, 0, PWM_L, IN1_L, IN2_L);
     }
-  
+    if (safePosR*encoderCircumferenceRatio <= L2)
+    {
+      setMotor(rightDir, 0, PWM_R, IN1_R, IN2_R);
+    }
   }
   
   Serial.println("Outside while loop");
 
-
   jobDone = true;
   }
 }
-}
+
 
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
   analogWrite(pwm,pwmVal);
